bc65857477785b56ca44e1a08928578cc66dbcf7
unknown <mugadzap@gmail.com>
2025-10-15 16:34:18 +0100

    working on product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Products from './components/Products'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 class="flex items-center justify-center h-screen
-     bg-gray-800 text-white text-2xl font-bold">
-    Welcome
-  </h1>
+    <div>
+    <Products />
+    </div>
   )
 }
 
